6da09c7f1c1bb34993f90264e33fbc848f081def
IT <jaydeep@skino.local>
2025-11-15 17:28:42 +0530

    05 - 11 - 2025

---
+++ LISTS.py
@@ -0,0 +1,40 @@
+list1 = [1,2,3,4,5,6,7,8,9,10]
+print(list1)
+
+# NOTE - List items are ordered, changeable, and allow duplicate values.
+
+# allow duplicates
+list2 = ["Radha","Radha","Radha","Radha","Radha","Radha"]
+print(list2)
+
+# len
+list3 = ["Radha","Krishna","Parvati","Shiv"]
+print(list3)
+print(len(list3))
+
+# list items / data types
+list4 = [1,2,3,4,5,6,7,8,9,10] 
+list5 = [True, False, True,False]
+list6 = ["Radha","Krishna","Parvati","Shiv","Saraswati","Brahma"]
+print(list4)
+print(list5)
+print(list6)
+
+# Even a list can contain different data types
+list7 = ["Radha",10,"Krishna",True]
+print(list7)
+
+# type
+list8 = ["Radha","Radha","Radha","Radha","Radha"]
+print(type(list8))
+
+# Constructor
+list9 = list(("Radha","Radha","Radha","Radha"))
+print(list9)
+
+# Access list items
+list10 = ["Radha",10,True,"Krishna",5.6]
+print(list10[4])
+# 
+list11 = ["Radha",10,True,"Krishna",5.6]
+print(list11[-1])
