b0ee5e85b8e8663178ef6d4d544d48b92c9ec565
IT <jaydeep@skino.local>
2025-11-20 17:29:05 +0530

    20 - 11 - 2025

--- LISTS.py
+++ LISTS.py
@@ -197,3 +197,43 @@ O = [1,2,3]
 print(O)
 O.clear()
 print(O)
+
+# Looping through  a list 
+P = [1,2,3,4,5,6,7,8,9,10]
+for Q in P:
+	print(Q)
+
+# Loop Through the index numbers
+R = ["Radha","Radha","Radha"]
+for S in range(len(R)):
+	print(R[S])
+
+# Use while loop in list
+T = ["Radha","Krishna","Parvati","Shiv"]
+U = 0
+while U < len(T):
+	print(T[U])
+	U += 1
+
+# List Comprehension
+# List Comprehension offers the shortest syntax for looping through lists:
+
+print("-" * 50)
+List_1 = ["Radha","Radha","Radha","Radha"]
+[print(V) for V in List_1]
+
+# List comprehension offers a shorter syntax when you want to create a new list based on the values of an 
+# existing list.
+# Example:
+# Based on a list of fruits, you want a new list, containing only the fruits with the letter "a" in the name.
+# Without list comprehension you will have to write a for statement with a conditional test inside:
+god = ["Laxmi","Parvati","Saraswati","Vishnu","Mahesh","Brahma"]
+W = []
+for X in god:
+	if "a" in X:
+		W.append(X)
+print(W)
+
+# With list comprehension you can do all that with only one line of code:
+
+
